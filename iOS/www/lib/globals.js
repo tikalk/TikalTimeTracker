f6c2b53fa50310e70c11e34ac9f4029689e436c0
Dov Goldberg <dov@tikalk.com>
2012-04-30 12:20:18 +0300

    Add iOS Project

---
+++ iOS/www/lib/globals.js
@@ -0,0 +1,29 @@
+// JavaScript Document
+var fsqClientID = '1OYPMZW55HEI5CHOJ0AH4EGJATOF0TQD3Z03PRNAJZIKWTPM';
+var fsqRedirect = 'http://tikalk.com';
+
+						
+$('#map_page').live("pageshow", function() {
+	$('#map_canvas').gmap(
+		{ 'center' : new google.maps.LatLng(currentLocation.location.lat, currentLocation.location.lng), 
+		  'zoom' : 15, 
+		  'mapTypeControl' : true, 
+		  'navigationControl' : true,
+		  'scaleControl' : true,
+		  'zoomControl' : true,
+		  'streetViewControl' : false 
+		}
+	);
+                    
+	$('#map_canvas').gmap('refresh');
+    
+    
+	$('#map_canvas').gmap('clear', 'markers');
+
+	var marker = $('#map_canvas').gmap(
+		'addMarker',{ id:'m_1', 'position':  new google.maps.LatLng(currentLocation.location.lat, currentLocation.location.lng), 'bounds': true , 'animation' : 	google.maps.Animation.DROP}
+	)
+	.click(function() {
+		$('#map_canvas').gmap('openInfoWindow', { 'content': currentLocation.name }, this);
+	});
+});
